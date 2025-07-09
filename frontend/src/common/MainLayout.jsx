26ce9ccfc1711682e2156c12c8fc30a4256f3b17
yumin359 <yumin359@gmail.com>
2025-07-09 11:31:45 +0900

    0709 글작성 react bootstrap 적용

--- frontend/src/common/MainLayout.jsx
+++ frontend/src/common/MainLayout.jsx
@@ -1,15 +1,16 @@
 import { Outlet } from "react-router";
 import { AppNavBar } from "./AppNavBar.jsx";
+import { Container } from "react-bootstrap";
 
 export function MainLayout() {
   return (
     <div>
-      <div>
+      <div className="mb-3">
         <AppNavBar />
       </div>
-      <div>
+      <Container>
         <Outlet />
-      </div>
+      </Container>
     </div>
   );
 }
--- frontend/src/feature/board/BoardAdd.jsx
+++ frontend/src/feature/board/BoardAdd.jsx
@@ -2,6 +2,14 @@ import { useState } from "react";
 import { useNavigate } from "react-router";
 import axios from "axios";
 import { toast } from "react-toastify";
+import {
+  Button,
+  Col,
+  FormControl,
+  FormGroup,
+  FormLabel,
+  Row,
+} from "react-bootstrap";
 
 export function BoardAdd() {
   const [title, setTitle] = useState("");
@@ -38,34 +46,42 @@ export function BoardAdd() {
   }
 
   return (
-    <div>
-      <h3>글 작성</h3>
-      <div>
-        <input
-          type="text"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-      </div>
-      <div>
-        <textarea
-          id=""
-          cols="30"
-          rows="10"
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-        ></textarea>
-      </div>
-      <div>
-        <input
-          type="text"
-          value={author}
-          onChange={(e) => setAuthor(e.target.value)}
-        />
-      </div>
-      <div>
-        <button onClick={handleSaveButtonClick}>저장</button>
-      </div>
-    </div>
+    <Row className="justify-content-center">
+      <Col xs={12} md={8} lg={6}>
+        <h2 className="mb-4">글 작성</h2>
+        <div>
+          <FormGroup className="mb-3" controlId="title1">
+            <FormLabel>제목</FormLabel>
+            <FormControl
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </FormGroup>
+        </div>
+        <div>
+          <FormGroup className="mb-3" controlId="content1">
+            <FormLabel>본문</FormLabel>
+            <FormControl
+              as="textarea"
+              rows={6}
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+            />
+          </FormGroup>
+        </div>
+        <div>
+          <FormGroup className="mb-3" controlId="author1">
+            <FormLabel>작성자</FormLabel>
+            <FormControl
+              value={author}
+              onChange={(e) => setAuthor(e.target.value)}
+            />
+          </FormGroup>
+        </div>
+        <div className="mb-3">
+          <Button onClick={handleSaveButtonClick}>저장</Button>
+        </div>
+      </Col>
+    </Row>
   );
 }

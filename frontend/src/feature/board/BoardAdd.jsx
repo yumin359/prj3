d3bda3996f5aeb779bd97fad7434692b370ce8a5
yumin359 <yumin359@gmail.com>
2025-07-09 11:49:09 +0900

    0709 제목, 본문, 작성자 안 쓰면 저장 버튼 비활성화

--- frontend/src/feature/board/BoardAdd.jsx
+++ frontend/src/feature/board/BoardAdd.jsx
@@ -50,6 +50,18 @@ export function BoardAdd() {
       });
   }
 
+  // 제목, 본문, 작성자, 썼는지
+  let validate = true;
+  if (title.trim() === "") {
+    validate = false;
+  }
+  if (content.trim() === "") {
+    validate = false;
+  }
+  if (author.trim() === "") {
+    validate = false;
+  }
+
   return (
     <Row className="justify-content-center">
       <Col xs={12} md={8} lg={6}>
@@ -84,7 +96,10 @@ export function BoardAdd() {
           </FormGroup>
         </div>
         <div className="mb-3">
-          <Button onClick={handleSaveButtonClick} disabled={isProcessing}>
+          <Button
+            onClick={handleSaveButtonClick}
+            disabled={isProcessing || !validate}
+          >
             {isProcessing && <Spinner size="sm" />}
             {isProcessing || "저장"}
           </Button>

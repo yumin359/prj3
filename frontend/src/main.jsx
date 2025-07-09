a0fe4cf94ba31da64a824df1ff091042bdef9441
yumin359 <yumin359@gmail.com>
2025-07-09 10:35:08 +0900

    0709

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,6 @@ import { ToastContainer } from "react-toastify";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-    <ToastContainer />;
+    <ToastContainer />
   </StrictMode>,
 );

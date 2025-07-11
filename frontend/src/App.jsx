e9ca6fa4989b3d98445d12c3623ef070f189128c
yumin359 <yumin359@gmail.com>
2025-07-11 11:49:12 +0900

    0711 jwt login 배우는중 - localStorage에 토큰 저장, 삭제(logout)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,25 @@ import axios from "axios";
 
 function App() {
   function handleButton1Click() {
-    axios.post("/api/learn/jwt/sub1", {
-      email: "son@son.com",
-      password: "son",
-    });
+    axios
+      .post("/api/learn/jwt/sub1", {
+        email: "son@son.com",
+        password: "son",
+      })
+      .then((res) => {
+        console.log(res.data);
+        localStorage.setItem("token", res.data.token);
+      });
+  }
+
+  function handleButton2Click() {
+    localStorage.removeItem("token");
   }
 
   return (
     <div>
       <h3>jwt 로그인 연습</h3>
+      <button onClick={handleButton2Click}>2. token 지우기 (logout)</button>
       <button onClick={handleButton1Click}>1. token 얻기 (login)</button>
     </div>
   );

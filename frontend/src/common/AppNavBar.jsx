af5b8ee2597e60abaa51bbea2554cdd496c43443
yumin359 <yumin359@gmail.com>
2025-07-11 16:22:56 +0900

    0711 navbar에 현재 로그인한 사용자 출력

--- frontend/src/common/AppNavBar.jsx
+++ frontend/src/common/AppNavBar.jsx
@@ -1,7 +1,11 @@
 import { Link, NavLink } from "react-router";
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { useContext } from "react";
+import { AuthenticationContext } from "./AuthenticationContextProvider.jsx";
 
 export function AppNavBar() {
+  const { user } = useContext(AuthenticationContext);
+
   return (
     <div>
       <Navbar expand="lg" className="bg-body-tertiary">
@@ -32,6 +36,11 @@ export function AppNavBar() {
               <Nav.Link as={NavLink} to="/logout">
                 로그아웃
               </Nav.Link>
+              {user !== null && (
+                <Nav.Link as={NavLink} to={`/member?email=${user.email}`}>
+                  {user.nickName}
+                </Nav.Link>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>

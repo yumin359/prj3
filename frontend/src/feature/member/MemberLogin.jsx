c64bf2d0ee29983490bae5f54b714d03f8ee959e
yumin359 <yumin359@gmail.com>
2025-07-11 15:22:56 +0900

    0711 로그인 화면

--- frontend/src/feature/member/MemberLogin.jsx
+++ frontend/src/feature/member/MemberLogin.jsx
@@ -45,25 +45,25 @@ export function MemberLogin() {
 
   return (
     <Row className="justify-content-center">
-      <Col xs={12} md={8} lg={6}></Col>
-      <h2 className="mb-4">로그인</h2>
-      <FormGroup>
-        <FormLabel className="mb-3" controlId="email1">
-          이메일
-        </FormLabel>
-        <FormControl value={email} onChange={(e) => setEmail(e.target.value)} />
-      </FormGroup>
-      <FormGroup>
-        <FormLabel className="mb-3" controlId="password1">
-          암호
-        </FormLabel>
-        <FormControl
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </FormGroup>
-      <Button onClick={handleLogInButtonClick}>로그인</Button>
+      <Col xs={12} md={8} lg={6}>
+        <h2 className="mb-4">로그인</h2>
+        <FormGroup controlId="email1" className="mb-3">
+          <FormLabel>이메일</FormLabel>
+          <FormControl
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </FormGroup>
+        <FormGroup controlId="password1" className="mb-3">
+          <FormLabel>암호</FormLabel>
+          <FormControl
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </FormGroup>
+        <Button onClick={handleLogInButtonClick}>로그인</Button>
+      </Col>
     </Row>
   );
 }

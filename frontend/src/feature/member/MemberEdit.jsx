71298c154837dc7d8b7b0c5ab0a043ef10cff426
yumin359 <yumin359@gmail.com>
2025-07-11 09:29:10 +0900

    0711 암호변경, 암호 변경 버튼 비활성화 여부

--- frontend/src/feature/member/MemberEdit.jsx
+++ frontend/src/feature/member/MemberEdit.jsx
@@ -4,6 +4,7 @@ import {
   FormControl,
   FormGroup,
   FormLabel,
+  FormText,
   Modal,
   Row,
   Spinner,
@@ -17,6 +18,10 @@ export function MemberEdit() {
   const [member, setMember] = useState(null);
   const [modalShow, setModalShow] = useState(false);
   const [password, setPassword] = useState("");
+  const [passwordModalShow, setPasswordModalShow] = useState(false);
+  const [oldPassword, setOldPassword] = useState("");
+  const [newPassword1, setNewPassword1] = useState("");
+  const [newPassword2, setNewPassword2] = useState("");
 
   const [params] = useSearchParams();
 
@@ -64,6 +69,26 @@ export function MemberEdit() {
       });
   }
 
+  // 암호 변경 버튼 활성화 여부
+  let changePasswordButtonDisable = false;
+  let passwordConfirm = true;
+
+  if (oldPassword === "") {
+    changePasswordButtonDisable = true;
+  }
+  if (newPassword1 === "") {
+    changePasswordButtonDisable = true;
+  }
+  if (newPassword2 === "") {
+    changePasswordButtonDisable = true;
+  }
+  if (newPassword1 !== newPassword2) {
+    changePasswordButtonDisable = true;
+    passwordConfirm = false;
+  }
+
+  function handleChangePasswordButtonClick() {}
+
   if (!member) {
     return <Spinner />;
   }
@@ -77,6 +102,14 @@ export function MemberEdit() {
             <FormLabel>이메일</FormLabel>
             <FormControl disabled value={member.email} />
           </FormGroup>
+        </div>
+        <div className="mb-4">
+          <Button
+            variant="outline-info"
+            onClick={() => setPasswordModalShow(true)}
+          >
+            암호 변경
+          </Button>
         </div>
         <div>
           <FormGroup controlId="nickName1" className="mb-3">
@@ -147,6 +180,65 @@ export function MemberEdit() {
           <Button variant="primary" onClick={handleSaveButtonClick}>
             {/* 위의 메소드 실행되어 최종 탈퇴됨 */}
             저장
+          </Button>
+        </Modal.Footer>
+
+        {/* 암호 변경 모달 */}
+        {/*모달로 했지만 다른 페이지로 빼도 괜찮을 것 같아여*/}
+      </Modal>
+      <Modal
+        show={passwordModalShow}
+        onHide={() => setPasswordModalShow(false)}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>암호 변경</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <FormGroup className="mb-3" controlId="password1">
+            <FormLabel>현재 암호</FormLabel>
+            <FormControl
+              type="password"
+              value={oldPassword}
+              onChange={(e) => setOldPassword(e.target.value)}
+            />
+          </FormGroup>
+          <FormGroup className="mb-3" controlId="password2">
+            <FormLabel>변경할 암호</FormLabel>
+            <FormControl
+              type="password"
+              value={newPassword1}
+              onChange={(e) => setNewPassword1(e.target.value)}
+            />
+          </FormGroup>
+          <FormGroup className="mb-3" controlId="password3">
+            <FormLabel>변경할 암호 확인</FormLabel>
+            <FormControl
+              type="password"
+              value={newPassword2}
+              onChange={(e) => setNewPassword2(e.target.value)}
+            />
+            {passwordConfirm || (
+              <FormText className="text-danger">
+                패스워드가 일치하지 않습니다.
+              </FormText>
+            )}
+          </FormGroup>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="outline-dark"
+            onClick={() => setPasswordModalShow(false)}
+          >
+            {/* 모달이 닫힘 */}
+            취소
+          </Button>
+          <Button
+            disabled={changePasswordButtonDisable}
+            variant="primary"
+            onClick={handleChangePasswordButtonClick}
+          >
+            {/* 위의 메소드 실행되어 최종 탈퇴됨 */}
+            변경
           </Button>
         </Modal.Footer>
       </Modal>

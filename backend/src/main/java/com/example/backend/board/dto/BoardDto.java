961f5c1232dde2a9fd85537335885485e1fab594
yumin359 <yumin359@gmail.com>
2025-07-14 11:30:21 +0900

    0714 게시물 하나 보기 JPQL로 변경

--- backend/src/main/java/com/example/backend/board/dto/BoardDto.java
+++ backend/src/main/java/com/example/backend/board/dto/BoardDto.java
@@ -1,11 +1,15 @@
 package com.example.backend.board.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 // 게시물 하나보기 위한 DTO
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BoardDto {
     private Integer id;
     private String title;
--- backend/src/main/java/com/example/backend/board/repository/BoardRepository.java
+++ backend/src/main/java/com/example/backend/board/repository/BoardRepository.java
@@ -28,17 +28,20 @@ public interface BoardRepository extends JpaRepository<Board, Integer> {
             """)
     List<BoardListDto> findAllBy();
 
+    // 순서가 같아야 함 BoradDto에서 쓴 거랑
+    // 얘도 JPQL로 바꿈
     // 게시물 하나 보기 쿼리
     @Query(value = """
-                SELECT b.id id,
-                       b.title title,
-                       b.content content,
-                       b.inserted_at inserted_at,
-                       m.email author_email,
-                       m.nick_name author_nick_name
-                FROM board b JOIN member m 
-                    ON b.author = m.email
+                SELECT new com.example.backend.board.dto.BoardDto
+                     ( b.id,
+                       b.title,
+                       b.content,
+                       m.email,
+                       m.nickName,
+                       b.insertedAt)
+                FROM Board b JOIN Member m
+                    ON b.author.email = m.email
                 WHERE b.id = :id
-            """, nativeQuery = true)
+            """)
     BoardDto findBoardById(Integer id);
 }

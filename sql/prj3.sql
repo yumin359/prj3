7d29e85a096a12676e45f56648f1064951742c2b
yumin359 <yumin359@gmail.com>
2025-07-14 10:13:41 +0900

    0714 게시물 테이블 변경

--- sql/prj3.sql
+++ sql/prj3.sql
@@ -4,10 +4,13 @@ CREATE TABLE board
     id          INT AUTO_INCREMENT NOT NULL,
     title       VARCHAR(300)       NOT NULL,
     content     VARCHAR(10000)     NOT NULL,
-    author      VARCHAR(100)       NOT NULL,
+    author      VARCHAR(255)       NOT NULL,
     inserted_at datetime           NOT NULL DEFAULT NOW(),
-    CONSTRAINT pk_board PRIMARY KEY (id)
+    CONSTRAINT pk_board PRIMARY KEY (id),
+    FOREIGN KEY (author) REFERENCES member (email)
 );
+
+DROP TABLE board;
 
 # 회원 테이블
 CREATE TABLE member

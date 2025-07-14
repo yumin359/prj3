80c4a6c1c5d4631a33245500d464f92e7192c2c7
yumin359 <yumin359@gmail.com>
2025-07-14 15:12:11 +0900

    0714 주소

--- frontend/src/common/AuthenticationContextProvider.jsx
+++ frontend/src/common/AuthenticationContextProvider.jsx
@@ -36,9 +36,17 @@ export function AuthenticationContextProvider({ children }) {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
+    // 얘는 경로가 바뀌는 게 아니라 새로고침 할 때 한번 실행
+    console.log("새로고침");
+    // 마운트 될 때 실행
+    // 각 브라우저마다 localStorage가 있는데 거기서 token을 가져옴
     const token = localStorage.getItem("token");
     if (token) {
+      // 토큰을 가져오면
+      // 디코더(복호화)를 통해 평문으로 만들어서
       const payload = jwtDecode(token);
+      // 그 평문의 sub를 보내서 맞는지 확인하고? 값을 담음..?
+      // 얘는 그냥 정보 하나보기 에서 값 받아온것!! 경로가 같아서용
       axios.get("/api/member?email=" + payload.sub).then((res) => {
         // email
         // nickName
@@ -55,12 +63,14 @@ export function AuthenticationContextProvider({ children }) {
     localStorage.setItem("token", token);
     const payload = jwtDecode(token);
     axios.get("/api/member?email=" + payload.sub).then((res) => {
+      // 로그인 요청이 성공적으로 되면 user 라는 상태에
       // email
       // nickName
+      // scope을 넣음
       setUser({
         email: res.data.email,
         nickName: res.data.nickName,
-        scope: payload.scp.split(" "),
+        scope: payload.scp.split(" "), // scope은 여러 값이 넘어올 수 있음
       });
     });
   }
@@ -72,11 +82,14 @@ export function AuthenticationContextProvider({ children }) {
   }
 
   // hasAccess
+  // 로그인이 성공적으로 되면 그 정보에서
+  // 본인 정보/글만 수정/삭제 등 가능하게 본인인지 확인하는 메소드
   function hasAccess(email) {
     return user && user.email === email;
   }
 
   // isAdmin
+  // scope이 admin인지 확인하는 메소드
   function isAdmin() {
     return user && user.scope && user.scope.includes("admin");
   }

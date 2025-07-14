e4efc05250d415ff2842bff3df35301e47798007
yumin359 <yumin359@gmail.com>
2025-07-14 15:36:03 +0900

    0714 페이지 번호 클릭시 p(page) 요청 파라미터 전송됨

--- frontend/src/feature/board/BoardList.jsx
+++ frontend/src/feature/board/BoardList.jsx
@@ -1,4 +1,4 @@
-import { Col, Row, Spinner, Table } from "react-bootstrap";
+import { Col, Pagination, Row, Spinner, Table } from "react-bootstrap";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate, useSearchParams } from "react-router";
@@ -42,53 +42,83 @@ export function BoardList() {
     return <Spinner />;
   }
 
+  const pageNumbers = [];
+  for (let i = 1; i <= 10; i++) {
+    pageNumbers.push(i);
+  }
+
+  function handlePageNumberClick(pageNumber) {
+    // console.log(pageNumber + "페이지로 이동");
+    // navigate(`/?p=${pageNumber}`);
+    const nextSearchParams = new URLSearchParams(searchParams);
+    nextSearchParams.set("p", pageNumber);
+    setSearchParams(nextSearchParams);
+  }
+
   return (
-    <Row>
-      <Col>
-        <h2 className="mb-4">글 목록</h2>
-        {/*게시물 하나도 없으면 p문단 글 나옴*/}
-        {boardList.length > 0 ? (
-          <Table striped={true} hover={true}>
-            <thead>
-              <tr>
-                <th style={{ width: "90px" }}>#</th>
-                <th>제목</th>
-                <th
-                  className="d-none d-md-table-cell"
-                  style={{ width: "200px" }}
-                >
-                  작성자
-                </th>
-                <th
-                  className="d-none d-lg-table-cell"
-                  style={{ width: "200px" }}
-                >
-                  작성일시
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {boardList.map((board) => (
-                <tr
-                  key={board.id}
-                  style={{ cursor: "pointer" }}
-                  onClick={() => handleTableRowClick(board.id)}
-                >
-                  <td>{board.id}</td>
-                  <td>{board.title}</td>
-                  {/*<td className="d-none d-md-table-cell">{board.author}</td>*/}
-                  <td className="d-none d-md-table-cell">{board.nickName}</td>
-                  <td className="d-none d-lg-table-cell">{board.timesAgo}</td>
+    <>
+      <Row className="my-3">
+        <Col>
+          <Pagination>
+            {pageNumbers.map((pageNumber) => (
+              <Pagination.Item
+                key={pageNumber}
+                onClick={() => handlePageNumberClick(pageNumber)}
+              >
+                {pageNumber}
+              </Pagination.Item>
+            ))}
+          </Pagination>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col>
+          <h2 className="mb-4">글 목록</h2>
+          {/*게시물 하나도 없으면 p문단 글 나옴*/}
+          {boardList.length > 0 ? (
+            <Table striped={true} hover={true}>
+              <thead>
+                <tr>
+                  <th style={{ width: "90px" }}>#</th>
+                  <th>제목</th>
+                  <th
+                    className="d-none d-md-table-cell"
+                    style={{ width: "200px" }}
+                  >
+                    작성자
+                  </th>
+                  <th
+                    className="d-none d-lg-table-cell"
+                    style={{ width: "200px" }}
+                  >
+                    작성일시
+                  </th>
                 </tr>
-              ))}
-            </tbody>
-          </Table>
-        ) : (
-          <p>
-            작성된 글이 없습니다. <br />새 글을 작성해 보세요.
-          </p>
-        )}
-      </Col>
-    </Row>
+              </thead>
+              <tbody>
+                {boardList.map((board) => (
+                  <tr
+                    key={board.id}
+                    style={{ cursor: "pointer" }}
+                    onClick={() => handleTableRowClick(board.id)}
+                  >
+                    <td>{board.id}</td>
+                    <td>{board.title}</td>
+                    {/*<td className="d-none d-md-table-cell">{board.author}</td>*/}
+                    <td className="d-none d-md-table-cell">{board.nickName}</td>
+                    <td className="d-none d-lg-table-cell">{board.timesAgo}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          ) : (
+            <p>
+              작성된 글이 없습니다. <br />새 글을 작성해 보세요.
+            </p>
+          )}
+        </Col>
+      </Row>
+    </>
   );
 }

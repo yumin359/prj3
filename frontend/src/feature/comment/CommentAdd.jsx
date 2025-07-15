7783aa49c51f851dbaf799c5c39fc0923b36fada
yumin359 <yumin359@gmail.com>
2025-07-15 09:24:11 +0900

    0715 댓글 후 toast

--- frontend/src/feature/comment/CommentAdd.jsx
+++ frontend/src/feature/comment/CommentAdd.jsx
@@ -1,6 +1,7 @@
 import { Button, FormControl } from "react-bootstrap";
 import { useState } from "react";
 import axios from "axios";
+import { toast } from "react-toastify";
 
 export function CommentAdd({ boardId }) {
   const [comment, setComment] = useState("");
@@ -11,10 +12,26 @@ export function CommentAdd({ boardId }) {
         boardId: boardId,
         comment: comment,
       })
-      .then((res) => {})
-      .catch((err) => {})
+      .then((res) => {
+        const message = res.data.message;
+        if (message) {
+          toast(message.text, { type: message.type });
+        }
+      })
+      .catch((err) => {
+        const message = err.response.data.message;
+        if (message) {
+          toast(message.text, { type: message.type });
+        }
+      })
       .finally(() => {});
   }
+
+  // TODO 댓글 비었을 때 저장 안 되게 -> 컨트롤러에서 막기
+
+  // TODO 로그인 했을 때만 댓글 활성화
+  // TODO 댓글 저장 버튼 여러 번 클릭되지 않게
+  // TODO 댓글 저장 후에 textarea 비우기
 
   return (
     <div>
@@ -24,7 +41,7 @@ export function CommentAdd({ boardId }) {
         value={comment}
         onChange={(e) => setComment(e.target.value)}
       />
-      <Button onClick={handleCommentSaveClick}>댓글 저장</Button>;
+      <Button onClick={handleCommentSaveClick}>댓글 저장</Button>
     </div>
   );
 }

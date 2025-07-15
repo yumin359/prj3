2c0afa45d5fb6a19fbf3d06389f29940eca506bf
yumin359 <yumin359@gmail.com>
2025-07-15 15:51:53 +0900

    0715 댓글 목록

--- frontend/src/feature/comment/CommentItem.jsx
+++ frontend/src/feature/comment/CommentItem.jsx
@@ -3,6 +3,9 @@ import axios from "axios";
 import { toast } from "react-toastify";
 import {
   Button,
+  Card,
+  CardBody,
+  CardHeader,
   FormControl,
   FormGroup,
   FormLabel,
@@ -55,28 +58,39 @@ export function CommentItem({ comment, isProcessing, setIsProcessing }) {
   }
 
   return (
-    <div className="border m-3">
-      <div className="d-flex justify-content-between m-3">
-        <div>{comment.authorNickName}</div>
-        <div>{comment.timesAgo}</div>
+    <>
+      <div className="position-relative">
+        <Card className="my-3">
+          <CardHeader className="d-flex justify-content-between">
+            <div style={{ fontWeight: "bold" }}>{comment.authorNickName}</div>
+            <small>{comment.timesAgo}</small>
+          </CardHeader>
+          <CardBody>
+            <div style={{ whiteSpace: "pre" }}>{comment.comment}</div>
+          </CardBody>
+        </Card>
+        {hasAccess(comment.authorEmail) && (
+          <div className="position-absolute end-0 bottom-0 m-3">
+            <Button
+              size="sm"
+              variant="outline-danger"
+              disabled={isProcessing}
+              onClick={() => setDeleteModalShow(true)}
+              className="me-2"
+            >
+              {isProcessing && <Spinner size="sm" />}삭제
+            </Button>
+            <Button
+              size="sm"
+              variant="outline-primary"
+              disabled={isProcessing}
+              onClick={() => setEditModalShow(true)}
+            >
+              {isProcessing && <Spinner size="sm" />}수정
+            </Button>
+          </div>
+        )}
       </div>
-      <div>{comment.comment}</div>
-      {hasAccess(comment.authorEmail) && (
-        <div>
-          <Button
-            disabled={isProcessing}
-            onClick={() => setDeleteModalShow(true)}
-          >
-            {isProcessing && <Spinner size="sm" />}삭제
-          </Button>
-          <Button
-            disabled={isProcessing}
-            onClick={() => setEditModalShow(true)}
-          >
-            {isProcessing && <Spinner size="sm" />}수정
-          </Button>
-        </div>
-      )}
 
       {hasAccess(comment.authorEmail) && (
         <>
@@ -139,6 +153,6 @@ export function CommentItem({ comment, isProcessing, setIsProcessing }) {
           </Modal>
         </>
       )}
-    </div>
+    </>
   );
 }

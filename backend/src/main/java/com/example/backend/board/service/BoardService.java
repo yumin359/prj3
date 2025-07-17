887bc79c4bccda1576432ff2942dda88838e7cd9
yumin359 <yumin359@gmail.com>
2025-07-17 11:28:06 +0900

    0717 게시물 작성시 실제 파일 s3에 업로드

--- backend/src/main/java/com/example/backend/board/service/BoardService.java
+++ backend/src/main/java/com/example/backend/board/service/BoardService.java
@@ -120,37 +120,37 @@ public class BoardService {
                     boardFileRepository.save(boardFile);
 
                     // 실제 파일 disk에 저장
-                    // todo : aws s3 에 저장을 변경할 예정
                     // 1. C:/Temp/prj3/boardFile 에 게시물 번호 폴더 만들고
                     /// C:/Temp/prj3/boardFile/2002
-                    File folder = new File("C:/Temp/prj3/boardFile/" + board.getId());
-                    if (!folder.exists()) {
-                        folder.mkdirs();
-                    }
-
-
+//                    File folder = new File("C:/Temp/prj3/boardFile/" + board.getId());
+//                    if (!folder.exists()) {
+//                        folder.mkdirs();
+//                    }
                     // 2. 그 폴더에 파일 저장
                     /// C:/Temp/prj3/boardFile/2002/tiger.jpg
-                    try {
+//                    try {
+//
+//                        BufferedInputStream bi = new BufferedInputStream(file.getInputStream());
+//                        BufferedOutputStream bo
+//                                = new BufferedOutputStream(new FileOutputStream(new File(folder, file.getOriginalFilename())));
+//
+//                        try (bi; bo) {
+//                            byte[] b = new byte[1024];
+//                            int len;
+//                            while ((len = bi.read(b)) != -1) {
+//                                bo.write(b, 0, len);
+//                            }
+//                            bo.flush();
+//
+//                        }
+//                    } catch (Exception e) {
+//                        e.printStackTrace();
+//                        throw new RuntimeException(e);
+//                    }
 
-                        BufferedInputStream bi = new BufferedInputStream(file.getInputStream());
-                        BufferedOutputStream bo
-                                = new BufferedOutputStream(new FileOutputStream(new File(folder, file.getOriginalFilename())));
-
-                        try (bi; bo) {
-                            byte[] b = new byte[1024];
-                            int len;
-                            while ((len = bi.read(b)) != -1) {
-                                bo.write(b, 0, len);
-                            }
-                            bo.flush();
-
-                        }
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        throw new RuntimeException(e);
-                    }
-
+                    // 실제 파일을 aws s3에 저장
+                    String objectKey = "prj3/board/" + board.getId() + "/" + file.getOriginalFilename();
+                    uploadFile(file, objectKey);
 
                 }
             }

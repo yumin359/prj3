6586cfcfa2f82591fba786a564bb113fc12082be
yumin359 <yumin359@gmail.com>
2025-07-17 09:19:54 +0900

    0717 폴더 안에 실제 파일 지우기

--- backend/src/main/java/com/example/backend/board/repository/BoardFileRepository.java
+++ backend/src/main/java/com/example/backend/board/repository/BoardFileRepository.java
@@ -4,6 +4,7 @@ import com.example.backend.board.entity.Board;
 import com.example.backend.board.entity.BoardFile;
 import com.example.backend.board.entity.BoardFileId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -11,4 +12,11 @@ public interface BoardFileRepository extends JpaRepository<BoardFile, BoardFileI
     List<BoardFile> findByBoardId(Integer id);
 
     void deleteByBoard(Board board);
+
+    @Query("""
+            SELECT f.id.name
+            FROM BoardFile f
+            WHERE f.board = :board
+            """)
+    List<String> listFileNameByBoard(Board board);
 }
--- backend/src/main/java/com/example/backend/board/service/BoardService.java
+++ backend/src/main/java/com/example/backend/board/service/BoardService.java
@@ -187,6 +187,17 @@ public class BoardService {
             // 좋아요 삭제
             boardLikeRepository.deleteByBoard(db);
 
+            // 디스크의 파일에 있는 거 먼저 삭제
+            ///  1. 파일 목록 얻고
+            List<String> fileNames = boardFileRepository.listFileNameByBoard(db);
+            /// 2. 파일 지우기
+            for (String fileName : fileNames) {
+                File f = new File("C:/Temp/prj3/boardFile/" + db.getId() + "/" + fileName);
+                if (f.exists()) {
+                    f.delete();
+                }
+            }
+
             // 파일 삭제
             boardFileRepository.deleteByBoard(db);
 

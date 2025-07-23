93b6775e9d922e0abaf812baef7538e354e0f0f2
yumin359 <yumin359@gmail.com>
2025-07-23 18:30:36 +0900

    memebr 나중에 아마 수정할 듯

--- backend/src/main/java/com/example/backend/member/entity/Member.java
+++ backend/src/main/java/com/example/backend/member/entity/Member.java
@@ -15,9 +15,11 @@ public class Member {
     @Id
     private String email;
 
-    private String password;
+    private String password; // 일반 로그인은 사용, 구글 로그인은 null
     private String nickName;
     private String info;
+
+//    private String provider; // "local" 또는 "google"
 
     @Column(insertable = false, updatable = false)
     private LocalDateTime insertedAt;
